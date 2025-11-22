f680519c901b99909c629d0e4ed40996fe4c21a8
Ryusei Igarashi <160300516+11gather11@users.noreply.github.com>
2025-11-22 21:32:54 +0900

    feat: implement updateActivity function and integrate it into client and guild events (#9)

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
-import { ActivityType, Client, GatewayIntentBits } from 'discord.js'
+import { Client, GatewayIntentBits } from 'discord.js'
 import { loadCommands } from '@/handlers/commands'
 import { loadEvents } from '@/handlers/events'
-import version from '../package.json'
 
-// 新しいClientインスタンスを作成
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
-	presence: {
-		activities: [
-			{
-				name: `v${version.version}`,
-				type: ActivityType.Custom,
-			},
-		],
-		status: 'online',
-	},
 })
 
 client.commands = await loadCommands()
