d5cc3783bbd01b69da25fe1e58b76e2f655b8884
11gather11 <11gather11@gmail.com>
2026-01-06 15:16:59 +0900

    refactor: set name for help command in SlashCommandBuilder

--- src/commands/help/index.ts
+++ src/commands/help/index.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from 'discord.js'
 import type { Command } from '@/types/command'
 
 export default {
-	command: new SlashCommandBuilder(),
+	command: new SlashCommandBuilder().setName('help'),
 	execute(interaction) {
 		interaction.reply('Help command executed!')
 	},
