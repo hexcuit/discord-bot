52b7e2ff640dc5371ba1efb367e6278bb2bd6baf
11gather11 <11gather11@gmail.com>
2026-01-06 15:14:14 +0900

    refactor: remove unused help command implementation

--- src/commands/help copy/index.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js'
-import type { Command } from '@/types/command'
-
-export default {
-	command: new SlashCommandBuilder(),
-	execute(interaction) {
-		interaction.reply('Help command executed!')
-	},
-} satisfies Command
